d964ed2554389a4c22e5b27740cee716274b23a7
Karen <kmve88@mail.ru>
2024-03-30 13:11:24 +0400

    Create new static dir, connected this folder to my project, creat new view and HTML file -> "index".

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/index.html')
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/about.html')
