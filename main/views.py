d92b53da92efbc06b416b6ba425f9f6f3b533ed7
Karen <kmve88@mail.ru>
2024-03-30 16:19:42 +0400

    Create new HTML file "about.html" and made changes to the folder URLs

--- main/views.py
+++ main/views.py
@@ -5,8 +5,23 @@ from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, 'main/index.html')
+    context = {
+        'title': 'RegalRidge - Main',
+        'content': 'RegalRidge Furniture Store'
+    }
+
+    return render(request, 'main/index.html', context)
 
 
 def about(request: HttpRequest) -> HttpResponse:
-    return render(request, 'main/about.html')
+    context = {
+        'title': 'RegalRidge - About Us',
+        'content': 'About Us',
+        'text_on_page': '''
+        Welcome to RegalRidge! Discover stylish furniture for every room in your home.
+        From cozy sofas to elegant dining sets,
+        we have what you need to create your dream space.
+        Shop now and elevate your home with RegalRidge's exquisite collection.'''
+    }
+
+    return render(request, 'main/about.html', context)
