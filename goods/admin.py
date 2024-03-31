7317db59e5f100d979bf2f6d36c569ed5d92d6fd
Karen <kmve88@mail.ru>
2024-03-31 11:24:09 +0400

    Create models -> "Categories and 5 Products" and I filled them

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Categories,
+                     Products_kitchen,
+                     Products_bedroom,
+                     Products_livingroom,
+                     Products_office,
+                     Products_accessories,
+                     Products_decor)
+
+
+admin.site.register(Categories)
+admin.site.register(Products_kitchen)
+admin.site.register(Products_bedroom)
+admin.site.register(Products_livingroom)
+admin.site.register(Products_office)
+admin.site.register(Products_accessories)
+admin.site.register(Products_decor)
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,119 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'category'
+
+class Products_kitchen(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits=8, decimal_places=2)
+    discount = models.IntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        db_table = 'kitchen'
+
+
+class Products_bedroom(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits=8, decimal_places=2)
+    discount = models.IntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'bedroom'
+
+
+class Products_livingroom(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits=8, decimal_places=2)
+    discount = models.IntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'living_room'
+
+
+class Products_office(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits=8, decimal_places=2)
+    discount = models.IntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'office'
+
+
+class Products_accessories(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits=8, decimal_places=2)
+    discount = models.IntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'accessories'
+
+
+
+class Products_decor(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, null=True)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits=8, decimal_places=2)
+    discount = models.IntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'decor'
