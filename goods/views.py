ad183077e9a790d20b43678a0a12181825a7eb83
Karen <kmve88@mail.ru>
2024-04-02 17:14:58 +0400

    Add 5 images for bedroom and 5 images for living room.   add new filters, templatetags for pagination working together filters.

--- goods/views.py
+++ goods/views.py
@@ -14,11 +14,28 @@ class CategoryListView(ListView):
 
     def get_queryset(self):
         category_slug = self.kwargs.get('category_slug')
-        if category_slug == 'all':
-            queryset = super().get_queryset()
-        else:
-            queryset = get_list_or_404(super().get_queryset().filter(category__slug=category_slug))
+        on_sale = self.request.GET.get('on_sale', None)
+        order_by = self.request.GET.get('order_by', None)
+
+        queryset = super().get_queryset()
+
+        if category_slug != 'all':
+            queryset = queryset.filter(category__slug=category_slug)
+
+        if on_sale:
+            queryset = queryset.filter(discount__gt=0)
+
+        if order_by and order_by != 'default':
+            queryset = queryset.order_by(order_by)
+
         return queryset
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_slug'] = self.kwargs.get('category_slug', None)
+        return context
+
 
 class ProductDetailsView(DetailView):
     template_name = 'goods/product.html'
