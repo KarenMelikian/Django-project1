8cc83003dada8dbe06fb016ca1e454fcbdd1f650
Karen <kmve88@mail.ru>
2024-04-03 17:09:34 +0400

    created a new, complex SEARCH scheme

--- goods/utils.py
+++ goods/utils.py
@@ -13,16 +13,12 @@ def q_search(query):
         product_id = int(query)
         return Products.objects.filter(id=product_id)
 
-    return Products.objects.filter(description__search=query)
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
 
-
-
-    # keywords = [word for word in query.split() if len(word) > 2]
-    #
-    # q_objects = Q()
-    #
-    # for token in keywords:
-    #     q_objects |= Q(description__icontains=token)
-    #     q_objects |= Q(name__icontains=token)
-    #
-    # return Products.objects.filter(q_objects)
+    return Products.objects.annotate(
+        rank=SearchRank(
+            vector,
+            query
+        )
+    ).filter(rank__gt=0).order_by('-rank')
