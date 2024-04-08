0d21bd183246f10398a4d4c593df4ceaaddaf839
Karen <kmve88@mail.ru>
2024-04-08 13:59:31 +0400

    1)Create new model Cart and methods for it       2)Create Cart add view, new template tag -> "user_carts" and new include template -> "cart-button.html".

--- cart/urls.py
+++ cart/urls.py
@@ -4,7 +4,7 @@ from .views import CartAddView, CartChangeView, CartRemoveView
 app_name = 'cart'
 
 urlpatterns = [
-    path('cart-add/', CartAddView.as_view(), name='cart_add'),
-    path('cart-change/', CartChangeView.as_view(), name='cart_change'),
-    path('cart-remove/', CartRemoveView.as_view(), name='cart_remove'),
+    path('cart-add/<slug:product_slug>/', CartAddView.as_view(), name='cart_add'),
+    path('cart-change/<slug:product_slug>/', CartChangeView.as_view(), name='cart_change'),
+    path('cart-remove/<slug:product_slug>/', CartRemoveView.as_view(), name='cart_remove'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
-from django.views.generic import DeleteView, UpdateView, CreateView
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import DeleteView, UpdateView
 
-class CartAddView(CreateView):
-    template_name = 'cart/cart-add.html'
+from goods.models import Products
+
+from .models import Cart
+
+
+class CartAddView(View):
+    def get(self, request, product_slug):
+        product = Products.objects.get(slug=product_slug)
+
+        if request.user.is_authenticated:
+            carts = Cart.objects.filter(user=request.user, product=product)
+
+            if carts.exists():
+                cart = carts.first()
+                if cart:
+                    cart.quantity += 1
+                    cart.save()
+            else:
+                Cart.objects.create(user=request.user, product=product, quantity=1)
+
+        return redirect(request.META['HTTP_REFERER'])
 
 
 class CartChangeView(UpdateView):
