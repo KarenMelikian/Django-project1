8d899d2e120b1f91865f26374a2386d12709520c
KarenMelikian <karmelikyan05@gmail.com>
2024-05-18 21:03:25 +0400

    Create order is ready.

--- mysite/order/models.py
+++ mysite/order/models.py
@@ -13,14 +13,14 @@ class OrderItemQueryset(models.QuerySet):
             return 0
 
 class Order(models.Model):
-    user = models.ForeignKey(to=User, on_delete=models.SET_DEFAULT, blank=True, null=True, default=None)
-    created_at = models.DateTimeField(auto_now_add=True)
-    phone_number = models.CharField(max_length=15)
-    requires_delivery = models.BooleanField(default=False)
-    delivery_address = models.TextField(null=True, blank=True)
-    payment_on_get = models.BooleanField(default=False)
-    is_paid = models.BooleanField(default=False)
-    status = models.CharField(max_length=50)
+    user = models.ForeignKey(to=User, on_delete=models.SET_DEFAULT, blank=True, null=True, verbose_name="Пользователь", default=None)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания заказа")
+    phone_number = models.CharField(max_length=20, verbose_name="Номер телефона")
+    requires_delivery = models.BooleanField(default=False, verbose_name="Требуется доставка")
+    delivery_address = models.TextField(null=True, blank=True, verbose_name="Адрес доставки")
+    payment_on_get = models.BooleanField(default=False, verbose_name="Оплата при получении")
+    is_paid = models.BooleanField(default=False, verbose_name="Оплачено")
+    status = models.CharField(max_length=50, default='В обработке', verbose_name="Статус заказа")
 
     class Meta:
         db_table = 'order'
@@ -30,12 +30,14 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Products, on_delete=models.SET_DEFAULT, null=True, default=None)
-    name = models.CharField(max_length=150)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE, verbose_name="Заказ")
+    product = models.ForeignKey(to=Products, on_delete=models.SET_DEFAULT, null=True, verbose_name="Продукт", default=None)
+    name = models.CharField(max_length=150, verbose_name="Название")
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата продажи")
+
+
 
 
     class Meta:
